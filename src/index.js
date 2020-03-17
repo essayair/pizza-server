9b86b16886762dc5a21ed8543e25c982d476527a
Victor Li <essayair@hotmail.com>
2020-03-18 01:56:10 +1000

    port debug

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 require('dotenv').config()
 const express = require('express');
 require('express-async-errors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
 //import routes
 const routes = require('./routes');
 const { connectToDB } = require('./utils/db')
-
 const errorHandler = require('./middleware/errorHandler');
-const app = express();
-app.use(express.json());
 
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const morganLog =
+  process.env.NODE_ENV === 'production' ? morgan('common') : morgan('dev');
+
+app.use(helmet());
+app.use(morganLog);
+app.use(cors());
+app.use(express.json());
 
 app.use('/api', routes);
 app.use(errorHandler);
