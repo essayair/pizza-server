8272ba0ae9413b554c3e2019730f373ea0081c7f
Victor Li <essayair@hotmail.com>
2020-03-18 20:01:28 +1000

    port bug

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const errorHandler = require('./middleware/errorHandler');
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 const morganLog =
   process.env.NODE_ENV === 'production' ? morgan('common') : morgan('dev');
